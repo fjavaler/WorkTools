82d497672c779806155f874af8158d78ecfdb63c
Frederick Javalera <frederickjavalera@gmail.com>
2019-03-10 20:12:26 -0600

    Added code for finding save file. Begin next at TODO: Start here next

--- Work Tools/src/caseTime/CaseTimeCalculator.java
+++ Work Tools/src/caseTime/CaseTimeCalculator.java
@@ -1,39 +1,38 @@
 package caseTime;
 
 import java.awt.EventQueue;
-
 import javax.swing.JFrame;
 import java.awt.Color;
 import java.awt.Toolkit;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.SwingConstants;
 import java.awt.Font;
-import javax.swing.JSpinner;
-import javax.swing.SpinnerListModel;
-import javax.swing.SpinnerNumberModel;
 import javax.swing.JButton;
 import javax.swing.JTextField;
 import javax.swing.JComboBox;
-import java.awt.SystemColor;
+import javax.swing.JFileChooser;
+
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import javax.swing.JCheckBox;
 import java.awt.event.ItemListener;
+import java.io.File;
+import java.util.Scanner;
 import java.awt.event.ItemEvent;
 
 public class CaseTimeCalculator
 {
-
-    private JFrame frame;
-    private JTextField textField;
-    private JTextField textField_1;
-    private JTextField textField_2;
-    private JTextField textField_3;
-    private JTextField textField_4;
-    private JTextField textField_5;
-    private JTextField textField_6;
-    private JTextField textField_7;
+    private JFrame frmTimeCalculator;
+    private JTextField AddHoursOutput;
+    private JTextField addMinutesOutput;
+    private JTextField shiftStartHourInput;
+    private JTextField shiftStartMinuteInput;
+    private JTextField currentTimeHourInput;
+    private JTextField currentTimeMinutesInput;
+    private JTextField cumulativeHoursInput;
+    private JTextField cumulativeMinutesInput;
     protected String shiftStartHour;
     protected String shiftStartMinute;
     protected int shiftStartAmPm;
@@ -42,6 +41,7 @@ public class CaseTimeCalculator
     protected int currentTimeAmPm;
     protected String accruedTimeHours;
     protected String accruedTimeMinutes;
+    File saveFile = null;
 
     /**
      * Launch the application.
@@ -55,7 +55,7 @@ public class CaseTimeCalculator
 		try
 		{
 		    CaseTimeCalculator window = new CaseTimeCalculator();
-		    window.frame.setVisible(true);
+		    window.frmTimeCalculator.setVisible(true);
 		} catch (Exception e)
 		{
 		    e.printStackTrace();
@@ -66,9 +66,53 @@ public class CaseTimeCalculator
 
     /**
      * Create the application.
+     * @throws Exception 
      */
-    public CaseTimeCalculator()
+
+    public CaseTimeCalculator() throws Exception
     {
+	//tests read from file
+	saveFile = new File("src/saveFile.sav");
+//		Read the file to test if file was found.
+	Scanner sc = new Scanner(saveFile);
+	int counter = 0;
+	String lastLine = "";
+	while (sc.hasNextLine())
+	{
+	    lastLine = sc.nextLine();
+	    
+	    counter++;
+	}
+	//have final final line at this point
+//	System.out.println("last line: " + lastLine);
+//	System.out.println("count: " + counter);
+	
+	if (counter != 0)
+	{
+	    System.out.println("has data");
+	    //Read and load start time to shiftStart text fields
+	    String hoursInput;
+	    String minutesInput;
+	    String amPMInput;
+	    //TODO: Start here next
+	}
+	else
+	{
+	    System.out.println("doesn't have data");
+	    //Prompt user for start time
+	    String userInput = JOptionPane.showInputDialog(
+		    "What time did you start work today?\nFormat: HHMMXM\n\nValid Examples:\n0800AM\n1200PM");
+	    //Valid format: HHMMXM
+	    Boolean isFormattedCorrectly = userInput.matches("[0-9]{4}[A|P]M");
+	    if (isFormattedCorrectly == false)
+		JOptionPane.showMessageDialog(null,
+			"Your time is improperly formatted.\nPlease enter a valid time in the format, \"HHMMXM\",\nWhere H stands for hour, M stands for minute, and XM can either be AM or PM");
+	    else
+		JOptionPane.showMessageDialog(null, "Correctly formatted!");
+	}
+	sc.close();   
+	
+//	See if save file already exists
 	initialize();
     }
 
@@ -77,10 +121,7 @@ public class CaseTimeCalculator
      */
     private void initialize()
     {
-
-	/*
-	 * Constants
-	 */
+	// Constants
 	String[] amPmString = { "AM", "PM" };
 
 	String[] hours = new String[] { "01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12" };
@@ -93,22 +134,21 @@ public class CaseTimeCalculator
 	    else
 		minutes[i] = "" + i;
 	}
-	/*
-	 * end constants
-	 */
 
-	frame = new JFrame();
-	frame.setForeground(Color.BLACK);
-	frame.setBackground(Color.BLACK);
-	frame.setIconImage(Toolkit.getDefaultToolkit().getImage(CaseTimeCalculator.class
+	// Begin draw
+	frmTimeCalculator = new JFrame();
+	frmTimeCalculator.setTitle("Case TIme Calculator");
+	frmTimeCalculator.setForeground(Color.BLACK);
+	frmTimeCalculator.setBackground(Color.BLACK);
+	frmTimeCalculator.setIconImage(Toolkit.getDefaultToolkit().getImage(CaseTimeCalculator.class
 		.getResource("/com/sun/javafx/scene/control/skin/modena/dialog-information.png")));
-	frame.getContentPane().setBackground(Color.BLACK);
-	frame.getContentPane().setLayout(null);
+	frmTimeCalculator.getContentPane().setBackground(Color.BLACK);
+	frmTimeCalculator.getContentPane().setLayout(null);
 
 	JPanel panel = new JPanel();
 	panel.setBackground(Color.BLACK);
 	panel.setBounds(10, 11, 364, 95);
-	frame.getContentPane().add(panel);
+	frmTimeCalculator.getContentPane().add(panel);
 	panel.setLayout(null);
 
 	JLabel lblTimeCalculator = new JLabel("Time Calculator Tool");
@@ -125,11 +165,11 @@ public class CaseTimeCalculator
 	lblWhatTimeDid.setBounds(10, 36, 344, 14);
 	panel.add(lblWhatTimeDid);
 
-	JComboBox comboBox_2 = new JComboBox(amPmString);
-	comboBox_2.setEnabled(false);
-	comboBox_2.setBackground(Color.LIGHT_GRAY);
-	comboBox_2.setBounds(235, 59, 50, 20);
-	panel.add(comboBox_2);
+	JComboBox shiftStartComboBox = new JComboBox(amPmString);
+	shiftStartComboBox.setEnabled(false);
+	shiftStartComboBox.setBackground(Color.LIGHT_GRAY);
+	shiftStartComboBox.setBounds(235, 59, 50, 20);
+	panel.add(shiftStartComboBox);
 
 	JLabel label = new JLabel("Hour");
 	label.setHorizontalAlignment(SwingConstants.CENTER);
@@ -149,19 +189,19 @@ public class CaseTimeCalculator
 	lblAmpm_1.setBounds(235, 82, 46, 14);
 	panel.add(lblAmpm_1);
 
-	textField_2 = new JTextField();
-	textField_2.setEditable(false);
-	textField_2.setBackground(Color.LIGHT_GRAY);
-	textField_2.setBounds(85, 59, 50, 20);
-	panel.add(textField_2);
-	textField_2.setColumns(10);
+	shiftStartHourInput = new JTextField();
+	shiftStartHourInput.setEditable(false);
+	shiftStartHourInput.setBackground(Color.LIGHT_GRAY);
+	shiftStartHourInput.setBounds(85, 59, 50, 20);
+	panel.add(shiftStartHourInput);
+	shiftStartHourInput.setColumns(10);
 
-	textField_3 = new JTextField();
-	textField_3.setEditable(false);
-	textField_3.setBackground(Color.LIGHT_GRAY);
-	textField_3.setColumns(10);
-	textField_3.setBounds(157, 59, 50, 20);
-	panel.add(textField_3);
+	shiftStartMinuteInput = new JTextField();
+	shiftStartMinuteInput.setEditable(false);
+	shiftStartMinuteInput.setBackground(Color.LIGHT_GRAY);
+	shiftStartMinuteInput.setColumns(10);
+	shiftStartMinuteInput.setBounds(157, 59, 50, 20);
+	panel.add(shiftStartMinuteInput);
 
 	JCheckBox chckbxUpdate = new JCheckBox("Update");
 	chckbxUpdate.addItemListener(new ItemListener()
@@ -169,20 +209,19 @@ public class CaseTimeCalculator
 	    public void itemStateChanged(ItemEvent e)
 	    {
 		int state = e.getStateChange();
-		if(state == ItemEvent.DESELECTED)
+		if (state == ItemEvent.DESELECTED)
 		{
-		    textField_2.setEditable(false);
-		    textField_3.setEditable(false);
-		    comboBox_2.setEnabled(false);
-		}
-		else
+		    shiftStartHourInput.setEditable(false);
+		    shiftStartMinuteInput.setEditable(false);
+		    shiftStartComboBox.setEnabled(false);
+		} else
 		{
-		    textField_2.setEditable(true);
-		    textField_3.setEditable(true);
-		    comboBox_2.setEnabled(true);
-		    textField_2.setBackground(Color.white);
-		    textField_3.setBackground(Color.white);
-		    comboBox_2.setBackground(Color.white);
+		    shiftStartHourInput.setEditable(true);
+		    shiftStartMinuteInput.setEditable(true);
+		    shiftStartComboBox.setEnabled(true);
+		    shiftStartHourInput.setBackground(Color.white);
+		    shiftStartMinuteInput.setBackground(Color.white);
+		    shiftStartComboBox.setBackground(Color.white);
 		}
 	    }
 	});
@@ -192,7 +231,7 @@ public class CaseTimeCalculator
 	JPanel panel_1 = new JPanel();
 	panel_1.setBackground(Color.BLACK);
 	panel_1.setBounds(10, 115, 364, 67);
-	frame.getContentPane().add(panel_1);
+	frmTimeCalculator.getContentPane().add(panel_1);
 	panel_1.setLayout(null);
 
 	JLabel lblWhatTimeIs = new JLabel("What Time Is It Currently?");
@@ -214,10 +253,10 @@ public class CaseTimeCalculator
 	lblMinutes_1.setBounds(157, 52, 50, 14);
 	panel_1.add(lblMinutes_1);
 
-	JComboBox comboBoxAmPm = new JComboBox(amPmString);
-	comboBoxAmPm.setBackground(Color.WHITE);
-	comboBoxAmPm.setBounds(235, 26, 50, 20);
-	panel_1.add(comboBoxAmPm);
+	JComboBox amPmComboBox = new JComboBox(amPmString);
+	amPmComboBox.setBackground(Color.WHITE);
+	amPmComboBox.setBounds(235, 26, 50, 20);
+	panel_1.add(amPmComboBox);
 
 	JLabel lblAmpm = new JLabel("AM/PM");
 	lblAmpm.setForeground(new Color(255, 255, 255));
@@ -225,28 +264,28 @@ public class CaseTimeCalculator
 	lblAmpm.setBounds(235, 52, 50, 14);
 	panel_1.add(lblAmpm);
 
-	textField_4 = new JTextField();
-	textField_4.setColumns(10);
-	textField_4.setBounds(85, 26, 50, 20);
-	panel_1.add(textField_4);
+	currentTimeHourInput = new JTextField();
+	currentTimeHourInput.setColumns(10);
+	currentTimeHourInput.setBounds(85, 26, 50, 20);
+	panel_1.add(currentTimeHourInput);
 
-	textField_5 = new JTextField();
-	textField_5.setColumns(10);
-	textField_5.setBounds(157, 26, 50, 20);
-	panel_1.add(textField_5);
+	currentTimeMinutesInput = new JTextField();
+	currentTimeMinutesInput.setColumns(10);
+	currentTimeMinutesInput.setBounds(157, 26, 50, 20);
+	panel_1.add(currentTimeMinutesInput);
 
 	JPanel panel_2 = new JPanel();
 	panel_2.setBackground(Color.BLACK);
 	panel_2.setBounds(10, 194, 364, 67);
-	frame.getContentPane().add(panel_2);
+	frmTimeCalculator.getContentPane().add(panel_2);
 	panel_2.setLayout(null);
 
-	JLabel lblCummulativeUnsubmittedAccrued = new JLabel("Cummulative Unsubmitted Accrued Time");
-	lblCummulativeUnsubmittedAccrued.setFont(new Font("Tahoma", Font.BOLD, 11));
-	lblCummulativeUnsubmittedAccrued.setForeground(Color.WHITE);
-	lblCummulativeUnsubmittedAccrued.setHorizontalAlignment(SwingConstants.CENTER);
-	lblCummulativeUnsubmittedAccrued.setBounds(59, 0, 251, 20);
-	panel_2.add(lblCummulativeUnsubmittedAccrued);
+	JLabel lblCumulativeUnsubmittedAccrued = new JLabel("Cumulative Unsubmitted Accrued Time");
+	lblCumulativeUnsubmittedAccrued.setFont(new Font("Tahoma", Font.BOLD, 11));
+	lblCumulativeUnsubmittedAccrued.setForeground(Color.WHITE);
+	lblCumulativeUnsubmittedAccrued.setHorizontalAlignment(SwingConstants.CENTER);
+	lblCumulativeUnsubmittedAccrued.setBounds(59, 0, 251, 20);
+	panel_2.add(lblCumulativeUnsubmittedAccrued);
 
 	JLabel lblHours = new JLabel("Hours");
 	lblHours.setHorizontalAlignment(SwingConstants.CENTER);
@@ -260,20 +299,20 @@ public class CaseTimeCalculator
 	lblMinutes.setBounds(196, 52, 50, 14);
 	panel_2.add(lblMinutes);
 
-	textField_6 = new JTextField();
-	textField_6.setColumns(10);
-	textField_6.setBounds(120, 24, 50, 20);
-	panel_2.add(textField_6);
+	cumulativeHoursInput = new JTextField();
+	cumulativeHoursInput.setColumns(10);
+	cumulativeHoursInput.setBounds(120, 24, 50, 20);
+	panel_2.add(cumulativeHoursInput);
 
-	textField_7 = new JTextField();
-	textField_7.setColumns(10);
-	textField_7.setBounds(195, 24, 50, 20);
-	panel_2.add(textField_7);
+	cumulativeMinutesInput = new JTextField();
+	cumulativeMinutesInput.setColumns(10);
+	cumulativeMinutesInput.setBounds(195, 24, 50, 20);
+	panel_2.add(cumulativeMinutesInput);
 
 	JPanel panel_3 = new JPanel();
 	panel_3.setBackground(Color.BLACK);
 	panel_3.setBounds(10, 264, 364, 86);
-	frame.getContentPane().add(panel_3);
+	frmTimeCalculator.getContentPane().add(panel_3);
 	panel_3.setLayout(null);
 
 	JButton btnCalculate = new JButton("Calculate");
@@ -281,23 +320,29 @@ public class CaseTimeCalculator
 	{
 	    public void actionPerformed(ActionEvent arg0)
 	    {
-		// To-Do
-		shiftStartHour = textField_2.getText();
-		shiftStartMinute = textField_3.getText();
-		shiftStartAmPm = comboBox_2.getSelectedIndex();
+		/*
+		 * TODO: Get shift start time from save file, except on first run.
+		 */
+		shiftStartHour = shiftStartHourInput.getText();
+		shiftStartMinute = shiftStartMinuteInput.getText();
+		shiftStartAmPm = shiftStartComboBox.getSelectedIndex();
 		if (shiftStartAmPm == 0)
 		{
-		    System.out.println(shiftStartAmPm);
+		    System.out.println("AM");
 		} else if (shiftStartAmPm == 1)
 		{
-		    System.out.println(shiftStartAmPm);
+		    System.out.println("PM");
 		}
-		currentTimeHour = textField_4.getText();
-		currentTimeMinute = textField_5.getText();
-		currentTimeAmPm = comboBoxAmPm.getSelectedIndex();
-		accruedTimeHours = textField_6.getText();
-		accruedTimeMinutes = textField_7.getText();
-		// Todo: Perform calculation
+		currentTimeHour = currentTimeHourInput.getText();
+		currentTimeMinute = currentTimeMinutesInput.getText();
+		currentTimeAmPm = amPmComboBox.getSelectedIndex();
+		accruedTimeHours = cumulativeHoursInput.getText();
+		accruedTimeMinutes = cumulativeMinutesInput.getText();
+
+		/*
+		 * TODO: Perform calculation here
+		 */
+
 		System.out.println("StartTimeHour: " + shiftStartHour);
 		System.out.println("StartTimeMinute: " + shiftStartMinute);
 		System.out.println("StartTimeAmPm: " + shiftStartAmPm);
@@ -309,10 +354,20 @@ public class CaseTimeCalculator
 		System.out.println("accruedTimeHours: " + accruedTimeHours);
 		System.out.println("accruedTimeMinutes: " + accruedTimeMinutes);
 
-		//
-		textField.setText("test");
-		textField_1.setText("test");
+		/*
+		 * TODO: Update output
+		 */
+		AddHoursOutput.setText("test");
+		addMinutesOutput.setText("test");
 
+		if (chckbxUpdate.isSelected())
+		{
+		    // System.out.println("CheckBox is selected");
+		    /*
+		     * Save Time to save file
+		     */
+
+		}
 	    }
 
 	});
@@ -321,11 +376,11 @@ public class CaseTimeCalculator
 	btnCalculate.setBounds(137, 6, 89, 30);
 	panel_3.add(btnCalculate);
 
-	textField = new JTextField();
-	textField.setEditable(false);
-	textField.setBounds(49, 46, 36, 20);
-	panel_3.add(textField);
-	textField.setColumns(10);
+	AddHoursOutput = new JTextField();
+	AddHoursOutput.setEditable(false);
+	AddHoursOutput.setBounds(49, 46, 36, 20);
+	panel_3.add(AddHoursOutput);
+	AddHoursOutput.setColumns(10);
 
 	JLabel lblAdd = new JLabel("Add");
 	lblAdd.setFont(new Font("Tahoma", Font.PLAIN, 12));
@@ -341,11 +396,11 @@ public class CaseTimeCalculator
 	lblHoursAnd.setBounds(93, 45, 64, 20);
 	panel_3.add(lblHoursAnd);
 
-	textField_1 = new JTextField();
-	textField_1.setEditable(false);
-	textField_1.setBounds(162, 46, 36, 20);
-	panel_3.add(textField_1);
-	textField_1.setColumns(10);
+	addMinutesOutput = new JTextField();
+	addMinutesOutput.setEditable(false);
+	addMinutesOutput.setBounds(162, 46, 36, 20);
+	panel_3.add(addMinutesOutput);
+	addMinutesOutput.setColumns(10);
 
 	JLabel lblMinutes_2 = new JLabel("minutes to your case time");
 	lblMinutes_2.setFont(new Font("Tahoma", Font.PLAIN, 12));
@@ -353,7 +408,7 @@ public class CaseTimeCalculator
 	lblMinutes_2.setForeground(Color.WHITE);
 	lblMinutes_2.setBounds(203, 48, 151, 14);
 	panel_3.add(lblMinutes_2);
-	frame.setBounds(100, 100, 400, 400);
-	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	frmTimeCalculator.setBounds(100, 100, 400, 400);
+	frmTimeCalculator.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 }
